3a49853ef9129347dabd6e433ba59ede4d41ea48
Erica Little <eteney90@gmail.com>
2021-04-22 23:29:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,16 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    self.new(filename).gsub(" - ","")
+    array = filename.split("-") && filename.gsub(".mp3","") 
+
+    artist_name = array[0]
+    song_name = array[1]
+    genre_name = array[2]
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+    
   end
 
 end
