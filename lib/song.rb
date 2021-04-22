c1676a69394b16de32ea79b5e37e2f25eb620790
Erica Little <eteney90@gmail.com>
2021-04-22 22:25:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,8 @@ class Song
     @genre = genre
   end
 
-  def find_by_name(name)
-    self.all.detect{|a| a.name == name}
+  def self.find_by_name(name)
+    all.detect{|a| a.name == name}
   end
 
 end
