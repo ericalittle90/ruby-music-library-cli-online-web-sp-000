e79843e64d3929c3f2760bf6303882beacffdd27
Erica Little <eteney90@gmail.com>
2021-04-22 21:24:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,7 +29,7 @@ class Artist
   def add_song(song)
     #song.artist = self unless song.artist
     @songs << song
-    song.artist = self 
+    song.artist = self
     songs.each { |song| add_song(song) }
   end
 
